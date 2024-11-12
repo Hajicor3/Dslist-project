e1098f29cf8e16b22a61b1d19e6d49264ebfb1cd
Gabriel Miranda <gabriel.mirandawv@outlook.com>
2024-11-12 18:06:56 -0300

    Sql query, projection, find by list

--- src/main/java/com/hajicor3/dslist/dto/GameMinDTO.java
+++ src/main/java/com/hajicor3/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.hajicor3.dslist.dto;
 
 import com.hajicor3.dslist.entities.Game;
+import com.hajicor3.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 	
@@ -12,7 +13,15 @@ public class GameMinDTO {
 	
 	public GameMinDTO() {
 	}
-
+	
+	public GameMinDTO(GameMinProjection projection) {
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
+	}
+	
 	public GameMinDTO(Game entity) {
 		id = entity.getId();
 		title =entity.getTitle();
