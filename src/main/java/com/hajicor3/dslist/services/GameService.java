b29652267fe0a648aa2d58e2fa49aabc8238055f
Gabriel Miranda <gabriel.mirandawv@outlook.com>
2024-11-12 14:56:38 -0300

    GET game by id

--- src/main/java/com/hajicor3/dslist/services/GameService.java
+++ src/main/java/com/hajicor3/dslist/services/GameService.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.hajicor3.dslist.dto.GameDTO;
 import com.hajicor3.dslist.dto.GameMinDTO;
 import com.hajicor3.dslist.entities.Game;
 import com.hajicor3.dslist.repositories.GameRepository;
@@ -15,8 +19,16 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+		
 	}
 }
